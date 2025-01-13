59b24b34f42ca643d43a8cb0028201df0e3a4139
kate030797 <167605570+kate030797@users.noreply.github.com>
2025-01-13 21:04:19 +0100

    Dodanie nowego projektu w VisualStudio.
    Dodanie nowego projektu w VisualStudio z szablonu NUnit Test Project.

---
+++ TrelloTests.cs
@@ -0,0 +1,16 @@
+namespace TrelloTestSuite
+{
+    public class TrelloTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
